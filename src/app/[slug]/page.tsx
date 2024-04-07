681d65493da8cdc7e6ee17d869f2916609da99b7
nmelnychenko <mykolam@seekingalpha.com>
2024-04-07 23:11:27 +0300

    finish last content section

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,4 +1,7 @@
+import cn from "@/tools/cn";
+
 import { Container } from '@/components/Container';
+import { ArrowLink } from "@/components/ArrowLink";
 
 import { content } from './content';
 import styles from './InnerPage.module.scss';
@@ -34,6 +37,45 @@ export default async function Page({ params: { slug } }: PageParams) {
           <p className='text-primary text-sm lg:text-base'>
             {content[slug].subtitle}
           </p>
+
+          <h2 className='mt-6 mb-4 text-black font-bold text-base/[22px] lg:mt-8'>Для участі в конкурсі потрібно зробити кілька кроків:</h2>
+
+          <ul className='m-0 pl-4 text-black list-decimal text-base/[22px]'>
+            <li className='mb-2'>
+              Намалювати малюнок на папері на тему «Мистецтво, що рятує життя».
+              Немає значення, скільки вам років, вмієте ви малювати чи користуєтеся відповідним програмним
+              забезпеченням. Фантазія і почуття – ось що робить ескіз, постер, малюнок витвором мистецтва.
+            </li>
+
+            <li className='mb-2'>
+              Завантажити фото чи сканкопію малюнку на <a className="text-primary hover:underline" href="">сайт конкурсу</a>, вказавши ім’я, прізвище, вік,
+              місце постійного проживання та номер телефону.
+            </li>
+
+            <li className='mb-2'>
+              На конкурс можна подати один малюнок.
+            </li>
+
+            <li className='mb-2'>
+              Оригінал малюнку потрібно зберігати до оголошення фіналістів конкурсу.
+            </li>
+
+            <li>
+              Фіналісти конкурси мають надіслати оригінал малюнка організатору конкурсу.
+            </li>
+          </ul>
+
+          <ArrowLink
+            className={
+              cn(
+                'mt-10',
+                styles.arrowLink,
+              )
+            }
+            href=''
+          >
+            Взяти участь
+          </ArrowLink>
         </div>
       </Container>
     </main>
