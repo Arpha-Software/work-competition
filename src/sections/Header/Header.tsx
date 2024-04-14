7ef9e11eb0e65c1cf6c4bdd972d6a1659f6cd59e
PolivkaArsen <arsen.polivka@tetalab.com>
2024-04-14 13:02:56 +0300

    bug: header link added

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import cn from '@/tools/cn';
 
 import { Container } from '@/components/Container';
@@ -6,11 +7,11 @@ import styles from './Header.module.scss';
 
 export const Header = () => {
   return (
-    <header className='relative px-5 pt-7 pb-15 bg-primary rounded-b-lg sticky top-0 md:pt-4.5 z-10 overflow-hidden'>
+    <header className='px-5 pt-7 pb-15 bg-primary rounded-b-lg sticky top-0 md:pt-4.5 z-10 overflow-hidden'>
       <Container>
-        <div className='px-8 text-center text-white text-sm md:text-4.5xl/[56px]'>
+        <Link href="/" className='block px-8 text-center text-white text-sm md:text-4.5xl/[56px]'>
           Конкурс «Мистецтво безпеки праці»
-        </div>
+        </Link>
       </Container>
 
       <div className={cn(styles.pattern, 'absolute bottom-0 left-0 h-10 w-full')}></div>
